d81e8bf9e4bbe802917c82d76f9c50f42d0ad1ec
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2020-11-06 21:29:32 +0800

    Add setters and getters for Response and Feature

--- sources/data/Feature.java
+++ sources/data/Feature.java
@@ -3,4 +3,20 @@ package data;
 public class Feature {
     private Geometry geometry;
     private Properties properties;
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 }
--- sources/data/Response.java
+++ sources/data/Response.java
@@ -6,4 +6,20 @@ import java.util.Map;
 public class Response {
     private String type;
     private List<Feature> features;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
 }
