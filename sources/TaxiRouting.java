46cce514cfc30fb25c1759e98b7879f2517d25de
derrick-lim-2019 <61318000+derrick-lim-2019@users.noreply.github.com>
2020-11-09 17:10:53 +0800

    Utility methods created
    - Load supporting dataset
    - Create HashMap to store cluster number and all their associated addresses
    - Create HashMap to store cluster number and their respective reference points

--- sources/TaxiRouting.java
+++ sources/TaxiRouting.java
@@ -1,3 +1,25 @@
-public class TaxiRouting {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class TaxiRouting {
+    
+    public static void main(String[] args) {
+        HashMap<Integer, List<Address>> clusteredAddresses = new HashMap<>(); 
+
+        //Load all supporting dataset into arrayList
+        ArrayList<Address> supportingAddresses = new ArrayList<>();
+        FindClusters.loadAddresses(supportingAddresses);
+
+        //Initialize a HashMap of key-value pairs of ZoneNumber & all its addresses
+        clusteredAddresses = FindClusters.initHashMap(supportingAddresses);
+        HashMap<Integer, Address> referencePoints = new HashMap<>();
+
+        //Populate the HashMap with all Key-Value pairs
+        for (Integer i : clusteredAddresses.keySet()){
+            referencePoints.put(i, FindClusters.findReferencePoint(i, clusteredAddresses));
+        }
+
+        System.out.println(referencePoints);
+    }
 }
