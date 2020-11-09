14d6519759c233256118a2836fe68a154a57c5b4
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2020-11-09 18:06:04 +0800

    Update taxi routing for basic pre processing

--- sources/TaxiRouting.java
+++ sources/TaxiRouting.java
@@ -1,20 +1,70 @@
+import data.Feature;
+import data.Response;
+import data.Taxi;
+import data.Zone;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class TaxiRouting {
     public static void main(String[] args) {
         // -------------------------pre processing-------------------------------------
         // create global collection of taxi, taxiCollection
+        List<Taxi> availableTaxis = getAvailableTaxis();
+        if (availableTaxis == null) {
+            return;
+        }
+
 
         // get all zones (28 zones)
+        List<Zone> zones = new ArrayList<>();
 
         // create a empty queue of zones
+        Queue<Zone> deficitZonesQueue = new ArrayBlockingQueue<Zone>(28);
 
         // iterate through zones and call getDeficit() on all of them
         // if getDeficit() > 0, enqueue to queue
+        for (Zone zone : zones) {
+            if (zone.getDeficit() > 0) {
+                deficitZonesQueue.add(zone);
+            }
+        }
 
         // ----------------------- processing ------------------------
         // while queue is not empty, dequeue and process zone
-
+        while (deficitZonesQueue.size() > 0) {
+            Zone current = deficitZonesQueue.poll();
+            process(current);
+        }
         // processing zone:
         // for i in getDeficit, find closest taxi to zone in global collection
         //    mark taxi as isAssigned, remove taxi from original zone
+
+        //
+    }
+
+    public static List<Taxi> getAvailableTaxis() {
+        List<Taxi> availableTaxis = new ArrayList<>();
+
+        try {
+            Response response = TaxiData.getData();
+            List<Feature> features = response.getFeatures();
+            Feature feature = features.get(0);
+            List<List<Double>> coordinates = feature.getGeometry().getCoordinates();
+
+            for (int i = 0; i < coordinates.size(); i++) {
+                availableTaxis.add(new Taxi(coordinates.get(i), i));
+            }
+        } catch (Exception e) {
+            System.out.println("Error when accessing API");
+            return null;
+        }
+        return availableTaxis;
+    }
+
+    public static void process(Zone zone) {
+        //dosomething
     }
 }
