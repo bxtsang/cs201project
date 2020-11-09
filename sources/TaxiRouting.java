11a87ad5abc1b588e015a63c4184913413cf6336
derrick-lim-2019 <61318000+derrick-lim-2019@users.noreply.github.com>
2020-11-09 19:57:08 +0800

    Updating filename
    Added two additional requirements for AddressUtilities

--- sources/TaxiRouting.java
+++ sources/TaxiRouting.java
@@ -15,15 +15,15 @@ public class TaxiRouting {
 
         //Load all supporting dataset into arrayList
         ArrayList<Address> supportingAddresses = new ArrayList<>();
-        FindClusters.loadAddresses(supportingAddresses);
+        AddressUtilities.loadAddresses(supportingAddresses);
 
         //Initialize a HashMap of key-value pairs of ZoneNumber & all its addresses
-        clusteredAddresses = FindClusters.initHashMap(supportingAddresses);
+        clusteredAddresses = AddressUtilities.initHashMap(supportingAddresses);
         HashMap<Integer, Address> referencePoints = new HashMap<>();
 
         //Populate the HashMap with all Key-Value pairs
         for (Integer i : clusteredAddresses.keySet()){
-            referencePoints.put(i, FindClusters.findReferencePoint(i, clusteredAddresses));
+            referencePoints.put(i, AddressUtilities.findReferencePoint(i, clusteredAddresses));
         }
 
         System.out.println(referencePoints);
