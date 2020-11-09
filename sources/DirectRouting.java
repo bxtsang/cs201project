087e0a4340c837f6f26c884fc13d18305966ac68
Lindy Lim <lindylimlw@gmail.com>
2020-11-10 00:13:37 +0800

    Added mocking methods

--- sources/DirectRouting.java
+++ sources/DirectRouting.java
@@ -27,11 +27,52 @@ public class DirectRouting {
         //         surplus = zone;
         //     }
         // }
+
+        List<Zone> zonesMock = mockTaxiData();
         
-        // Many surplus zones, many deficit zones
-         List<Zone> zonesWithDeficit = new ArrayList<>(); // to be fetched
-         
         
+    }
+
+    private List<Zone> mockTaxiData() {
+        List<Zone> zones = mockZoneData();
+        // create surplus in zone1 (demand: 1, taxis: 2)
+        List<Double> coordinates = new ArrayList<>();
+        coordinates.add(103.5);
+        coordinates.add(170.5);
+        Taxi taxi1 = new Taxi(coordinates, 1);
+        Taxi taxi2 = new Taxi(coordinates, 2);
+
+        zones.get(0).addTaxi(taxi1);
+        zones.get(0).addTaxi(taxi2);
+
+        // create deficit in zone2 (demand: 10, taxis: 2)
+        Taxi taxi3 = new Taxi(coordinates, 3);
+        Taxi taxi4 = new Taxi(coordinates, 4);
+
+        zones.get(1).addTaxi(taxi3);
+        zones.get(1).addTaxi(taxi4);
+
+        return zones;
+    }
+
+    private List<Zone> mockZoneData() {
+        List<Double> coordinates = new ArrayList<>();
+        coordinates.add(103.5);
+        coordinates.add(170.5);
+        List<Zone> zones = new ArrayList<>();
+        Zone zone1 = new Zone(coordinates, 1);
+        Zone zone2 = new Zone(coordinates, 10);
+        Zone zone3 = new Zone(coordinates, 100);
+        Zone zone4 = new Zone(coordinates, 5);
+        Zone zone5 = new Zone(coordinates, 15);
+
+        zones.add(zone1);
+        zones.add(zone2);
+        zones.add(zone3);
+        zones.add(zone4);
+        zones.add(zone5);
+
+        return zones;
     }
 
     private HashMap<Integer, Address> getReferencePoints() {
@@ -50,23 +91,5 @@ public class DirectRouting {
 
         return referencePoints;
     }
-
-    private List<Zone> mockZoneData() {
-        List<Zone> zones = new ArrayList<>();
-        Zone zone1 = new Zone();
-        Zone zone2 = new Zone();
-        Zone zone3 = new Zone();
-        Zone zone4 = new Zone();
-        Zone zone5 = new Zone();
-
-        zones.add(zone1);
-        zones.add(zone2);
-        zones.add(zone3);
-        zones.add(zone4);
-        zones.add(zone5);
-
-        return zones;
-    }
-
 
 }
